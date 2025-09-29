23e8554bed192fd1c49eed61126590add292f93f
Milagros Alvarez <milyalvarez.26@gmail.com>
2025-09-28 22:58:19 -0300

    se agregan validaciones al inicio de sesión y se modifica nombre del boton para registrarse

--- AppTuHoraSalud/app/src/main/java/com/example/apptuhorasalud/InicioActivity.java
+++ AppTuHoraSalud/app/src/main/java/com/example/apptuhorasalud/InicioActivity.java
@@ -2,8 +2,12 @@ package com.example.apptuhorasalud;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +16,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class InicioActivity extends AppCompatActivity {
-
+    EditText txtCorreo, txtContraseña;
+    Button btnIngresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +28,17 @@ public class InicioActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        TextView tvRecuperar = findViewById(R.id.tvRecuperar);
+        txtCorreo = findViewById(R.id.txtCorreo);
+        txtContraseña = findViewById(R.id.txtContraseña);
+        btnIngresar = findViewById(R.id.btnRegistrarse);
         // btn Ingresar
-
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarCampos();
+            }
+        });
 
         // btn Registrar
         TextView btnRegistro = findViewById(R.id.btnRegistrar);
@@ -33,5 +46,35 @@ public class InicioActivity extends AppCompatActivity {
             Intent intent = new Intent(InicioActivity.this, RegistroActivity.class);
             startActivity(intent);
         });
+
+        tvRecuperar.setOnClickListener(v -> {
+            Intent intent = new Intent(InicioActivity.this, RecuperoActivity.class);
+            startActivity(intent);
+        });
     }
+    private void validarCampos() {
+        String correo = txtCorreo.getText().toString();
+        String contraseña = txtContraseña.getText().toString();
+        if (correo.isEmpty()) {
+            Toast.makeText(this, "El correo es obligatorio", Toast.LENGTH_SHORT).show();
+            //txtCorreo.setError("Correo requerido");
+            txtCorreo.requestFocus();
+            return;
+        }
+        // Valida el formato del email
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+            Toast.makeText(this, "Ingrese un correo válido", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (contraseña.isEmpty()) {
+            Toast.makeText(this, "Ingrese una contraseña", Toast.LENGTH_SHORT).show();
+            //txtContraseña.setError("Contraseña requerida");
+            txtContraseña.requestFocus();
+            return;
+        }
+
+        Intent intent = new Intent(InicioActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
